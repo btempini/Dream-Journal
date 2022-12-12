cbb90d1f5546ea386de9fb72fe01697cc46565c7
Bryan Tempini <mistatempini@gmail.com>
2022-12-12 11:33:48 -0500

    updated dev dependencies

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const session = require("express-session");
+const routes = require("./controllers");
+
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const app = express();
