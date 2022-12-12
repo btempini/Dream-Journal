be8de82b850adb7284bf58678a2c433447398700
max <maximiliangibes@gmail.com>
2022-12-12 11:20:25 -0700

    updates index.js in seeds dir to have all necessary relative path connections and their respective database.

--- seeds/index.js
+++ seeds/index.js
@@ -1,7 +1,10 @@
 const sequelize = require("../config/connection");
-const Dream = require('../models/Dream')
-const nightmareData = require("../models/Nightmare")
+const Dream = require("../models/Dream");
+const Nightmare = require("../models/Nightmare");
+const User = require("../models/User");
+const nightmareData = require("./nightmare-seeds.json");
 const dreamData = require("./dream-seeds.json");
+const userData = require("./user-seeds.json");
 
 const dreamDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -23,6 +26,17 @@ const nightmareDatabase = async () => {
 
   process.exit(0);
 };
+const userDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  process.exit(0);
+};
 
 dreamDatabase();
 nightmareDatabase();
+userData();
