b7fbed0759a1a2255ce563490e787f06349a5cd4
Bryan Tempini <mistatempini@gmail.com>
2022-12-13 15:33:24 -0500

    user and home routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,2 +1,29 @@
 const router = require("express").Router();
-const { Dream, Nightmare, User } = require("./api");
+const { Dream, Nightmare, User } = require("../models");
+
+router.get("/", async (req, res) => {
+  const allData = [];
+  const dreamsData = await Dream.findAll({ include: [{ model: User }] });
+  const nightmareData = await Nightmare.findAll({ include: [{ model: User }] });
+
+  allData.push(...dreamsData);
+  allData.push(...nightmareData);
+
+  res.json(allData);
+});
+
+router.get("/", async (req, res) => {
+  const nightmareData = await Nightmare.findAll();
+  res.json(nightmareData);
+});
+
+// router.get("/", async (req, res) => {
+//   const dreamData = await Dream.findAll().catch((err) => {
+//     res.json(err);
+//   });
+//   res.status(200).json(dreamData);
+//   const dream = dishData.map((dish) => dish.get({ plain: true }));
+//   res.render("all", { dream });
+// });
+
+module.exports = router;
