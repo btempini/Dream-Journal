e239f29d8a420d8f6f798a7afa59bc74dce7daad
Ramo <mramazant@gmail.com>
2022-12-13 23:53:54 -0700

    created routes for handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,2 +1,17 @@
 const router = require("express").Router();
 const { Dream, Nightmare, User } = require("./api");
+
+router.get("/posts", async (req, res) => {
+  res.render("postPage")
+});
+
+router.get("/", async (req, res) => {
+  res.render("homepage")
+});
+
+router.get("/dashboard", async (req, res) => {
+  res.render("userHome")
+});
+
+
+module.exports = router;
